c8026aa5709018be0af0669e13dce50e3dd14ea5
kstensing <stensingk@gmail.com>
2022-05-15 23:04:12 -0500

    finalized resolver

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -44,8 +44,8 @@ export const SAVE_BOOK = gql`
 `;
 
 export const REMOVE_BOOK = gql`
-  mutation saveBook($bookId: String!) {
-    saveBook(bookId: $bookId) {
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
       _id
       username
       email
